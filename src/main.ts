de8c55bcce68f72bad25a0f6f549d5b1e8120933
Dukagjin Zejnaj <dukagjin.zejnaj@seekers.ch>
2023-11-08 19:19:38 +0100

    css import edited

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ import './assets/css/media-queries.css';
 
 /* Bootstrap */
 import 'bootstrap/dist/css/bootstrap.css';
-import "bootstrap-icons/font/bootstrap-icons.css";
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import 'bootstrap/dist/js/bootstrap.js';
 
 const app = createApp(App);
